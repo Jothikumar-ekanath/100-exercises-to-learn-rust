6e7ced66f7d948b38f00ac6a37f4e45efc99ae9b
jothikumar <jothikumar.kuppaekanath@nike.com>
2025-03-05 23:26:30 -0800

    trait exercises'

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,75 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+#[derive(Debug, PartialEq)]
+pub struct SaturatingU16 {
+	value: u16,
+}
+
+impl SaturatingU16 {
+	pub fn new(value: u16) -> Self {
+		SaturatingU16 {
+			value: value,
+		}
+	}
+
+	pub fn saturating_add(&self, other: &SaturatingU16) -> SaturatingU16 {
+		let sum = self.value.saturating_add(other.value);
+		SaturatingU16::new(sum)
+	}
+}
+
+impl std::ops::Add for SaturatingU16 {
+	type Output = SaturatingU16;
+
+	fn add(self, other: SaturatingU16) -> SaturatingU16 {
+		self.saturating_add(&other)
+	}
+}
+
+impl std::ops::Add<u16> for SaturatingU16 {
+	type Output = SaturatingU16;
+
+	fn add(self, other: u16) -> SaturatingU16 {
+		self.saturating_add(&SaturatingU16::new(other))
+	}
+}
+
+impl std::ops::Add<&u16> for SaturatingU16 {
+	type Output = SaturatingU16;
+
+	fn add(self, other: &u16) -> SaturatingU16 {
+		self.saturating_add(&SaturatingU16::new(*other))
+	}
+}
+
+impl std::cmp::PartialEq<u16> for SaturatingU16 {
+	fn eq(&self, other: &u16) -> bool {
+		self.value == *other
+	}
+}
+
+
+
+impl From<u16> for SaturatingU16 {
+	fn from(value: u16) -> Self {
+		SaturatingU16::new(value)
+	}
+}
+impl From<u8> for SaturatingU16 {
+	fn from(value: u8) -> Self {
+		SaturatingU16::new(value as u16)
+	}
+}
+
+impl From<&u16> for SaturatingU16 {
+	fn from(value: &u16) -> Self {
+		SaturatingU16::new(*value)
+	}
+}
+impl From<&u8> for SaturatingU16 {
+	fn from(value: &u8) -> Self {
+		SaturatingU16::new(*value as u16)
+	}
+}
